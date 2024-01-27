554fc5152948d3412bc2c0204fe269b67d83179e
Scott Motte <mot@mot.la>
2024-01-27 09:20:21 -0800

    add helpful message if .env.vault exists but .env* does not

--- src/cli/actions/run.js
+++ src/cli/actions/run.js
@@ -88,7 +88,14 @@ async function run () {
           // missing .env
           case 'ENOENT':
             logger.warnv(`missing ${envFilepath} file (${filepath})`)
-            logger.help(`? in development: add one with [echo "HELLO=World" > .env] and re-run [dotenvx run -- ${commandArgs.join(' ')}]`)
+
+            const vaultFilepath = helpers.resolvePath('.env.vault')
+            if (fs.existsSync(vaultFilepath)) {
+              logger.help(`? in development: use [dotenvx decrypt] to decrypt .env.vault to .env and then re-run [dotenvx run -- ${commandArgs.join(' ')}]`)
+            } else {
+              logger.help(`? in development: add one with [echo "HELLO=World" > .env] and re-run [dotenvx run -- ${commandArgs.join(' ')}]`)
+            }
+
             logger.help('? for production: set [DOTENV_KEY] on your server and re-deploy')
             logger.help('? for ci: set [DOTENV_KEY] on your ci and re-build')
             break
