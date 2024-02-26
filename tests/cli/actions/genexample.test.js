54c43944eab0a70c5a02b6ac7273194fd01637c3
Scott Motte <mot@mot.la>
2024-02-25 18:41:14 -0800

    further tests

---
+++ tests/cli/actions/genexample.test.js
@@ -0,0 +1,33 @@
+const t = require('tap')
+const fs = require('fs')
+const sinon = require('sinon')
+const dotenv = require('dotenv')
+
+const main = require('../../../src/lib/main')
+const genexample = require('../../../src/cli/actions/genexample')
+
+t.test('genexample calls main.genexample', async ct => {
+  const stub = sinon.stub(main, 'genexample')
+  stub.returns({
+    envExampleFile: 'HELLO=""',
+    envFile: '.env.example',
+    exampleFilepath: '.env.example',
+    addedKeys: ['HELLO']
+  })
+
+  const fsStub = sinon.stub(fs, 'writeFileSync')
+
+  const optsStub = sinon.stub().returns({})
+  const fakeContext = {
+    opts: optsStub
+  }
+
+  // Call the ls function with the fake context
+  await genexample.call(fakeContext, '.')
+
+  t.ok(stub.called, 'main.genexample() called')
+  t.ok(fsStub.called, 'fs.writeFileSync() called')
+  stub.restore()
+
+  ct.end()
+})
---
+++ tests/cli/actions/ls.test.js
@@ -0,0 +1,24 @@
+const t = require('tap')
+const sinon = require('sinon')
+const dotenv = require('dotenv')
+
+const main = require('../../../src/lib/main')
+const ls = require('../../../src/cli/actions/ls')
+
+t.test('ls calls main.ls', async ct => {
+  const stub = sinon.stub(main, 'ls')
+  stub.returns({})
+
+  const optsStub = sinon.stub().returns({})
+  const fakeContext = {
+    opts: optsStub
+  }
+
+  // Call the ls function with the fake context
+  await ls.call(fakeContext, '.')
+
+  t.ok(stub.called, 'main.ls() called')
+  stub.restore()
+
+  ct.end()
+})
