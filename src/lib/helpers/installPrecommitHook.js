ccf29aab4e29089a3d6045a64f21d515e97dfa8b
Scott Motte <mot@mot.la>
2024-02-27 18:34:05 -0800

    decouple precommit logging from services

--- src/lib/helpers/installPrecommitHook.js
+++ src/lib/helpers/installPrecommitHook.js
@@ -1,8 +1,6 @@
 const fs = require('fs')
 const path = require('path')
 
-const logger = require('./../../shared/logger')
-
 const HOOK_SCRIPT = `#!/bin/sh
 
 if ! command -v dotenvx &> /dev/null
@@ -21,20 +19,30 @@ class InstallPrecommitHook {
   }
 
   run () {
+    let successMessage
+
     try {
       // Check if the pre-commit file already exists
       if (this._exists()) {
         // Check if 'dotenvx precommit' already exists in the file
-        if (!this._currentHook().includes('dotenvx precommit')) {
-          this._appendHook()
+        if (this._currentHook().includes('dotenvx precommit')) {
+          // do nothing
+          successMessage = `dotenvx precommit exists [${this.hookPath}]`
         } else {
-          logger.warnvp(`dotenvx precommit exists [${this.hookPath}]`)
+          this._appendHook()
+          successMessage = `dotenvx precommit appended [${this.hookPath}]`
         }
       } else {
         this._createHook()
+        successMessage = `dotenvx precommit installed [${this.hookPath}]`
+      }
+
+      return {
+        successMessage
       }
     } catch (err) {
-      logger.errorvp(`failed to modify pre-commit hook: ${err.message}`)
+      const error = new Error(`failed to modify pre-commit hook: ${err.message}`)
+      throw error
     }
   }
 
@@ -50,13 +58,11 @@ class InstallPrecommitHook {
     // If the pre-commit file doesn't exist, create a new one with the hookScript
     fs.writeFileSync(this.hookPath, HOOK_SCRIPT)
     fs.chmodSync(this.hookPath, '755') // Make the file executable
-    logger.successvp(`dotenvx precommit installed [${this.hookPath}]`)
   }
 
   _appendHook () {
     // Append 'dotenvx precommit' to the existing file
     fs.appendFileSync(this.hookPath, '\n' + HOOK_SCRIPT)
-    logger.successvp(`dotenvx precommit appended [${this.hookPath}]`)
   }
 }
 
