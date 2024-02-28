53c155c5dab9588a65799965baf89ed0216c6557
Scott Motte <mot@mot.la>
2024-02-27 22:20:34 -0800

    test showing failure of personal envs

--- tests/lib/helpers/dotenvVault.test.js
+++ tests/lib/helpers/dotenvVault.test.js
@@ -35,6 +35,36 @@ t.test('#run', ct => {
   ct.end()
 })
 
+t.test('#run (personal environment variable)', ct => {
+  const dotenvKey = 'dotenv://:key_ac300a21c59058c422c18dba8dc9892a537a63e156af14b5c5ef14810dc71f20@dotenvx.com/vault/.env.vault?environment=development'
+  const dotenvFiles = {
+    '.env': 'HELLO=World\n# personal.dotenvx.com\nHELLO=personal'
+  }
+  const dotenvKeys = {
+    DOTENV_KEY_DEVELOPMENT: dotenvKey
+  }
+  const dotenvVaults = {}
+
+  const dotenvVault = new DotenvVault(dotenvFiles, dotenvKeys, dotenvVaults)
+
+  const {
+    dotenvVaultFile,
+    addedVaults,
+    existingVaults,
+    addedDotenvFilenames
+  } = dotenvVault.run()
+
+  const parsed = dotenv.parse(dotenvVaultFile)
+  const decryptedContent = decrypt(parsed.DOTENV_VAULT_DEVELOPMENT, dotenvKey)
+
+  ct.same(decryptedContent, 'HELLO=World')
+  ct.same(addedVaults, ['DOTENV_VAULT_DEVELOPMENT'])
+  ct.same(existingVaults, [])
+  ct.same(addedDotenvFilenames, ['.env'])
+
+  ct.end()
+})
+
 t.test('#run (vault already exists with same value)', ct => {
   const dotenvFiles = {
     '.env': 'HELLO=World'
