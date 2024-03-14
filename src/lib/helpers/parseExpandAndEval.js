7f93d71bb76b06d7a99e03443371eba4250c2e1a
Scott Motte <mot@mot.la>
2024-03-14 11:08:43 -0700

    patch eval

--- src/lib/helpers/parseExpandAndEval.js
+++ src/lib/helpers/parseExpandAndEval.js
@@ -3,39 +3,33 @@ const dotenvExpand = require('dotenv-expand')
 const dotenvEval = require('./dotenvEval')
 
 function parseExpandAndEval (src, overload) {
+  // parse
   const parsed = dotenv.parse(src)
 
-  // consider moving this logic straight into dotenv-expand
-  let inputParsed = {}
-  if (overload) {
-    inputParsed = { ...process.env, ...parsed }
-  } else {
-    inputParsed = { ...parsed, ...process.env }
+  // eval parsed only. do NOT eval process.env ever
+  const inputParsed = {
+    processEnv: {},
+    parsed: parsed
   }
+  const evaled = dotenvEval.eval(inputParsed).parsed
 
+  // consider moving this logic straight into dotenv-expand
+  let evalParsed = {}
+  if (overload) {
+    evalParsed = { ...process.env, ...evaled }
+  } else {
+    evalParsed = { ...evaled, ...process.env }
+  }
   const expandPlease = {
     processEnv: {},
-    parsed: inputParsed
+    parsed: evalParsed
   }
   const expanded = dotenvExpand.expand(expandPlease).parsed
-
-  let inputExpanded = {}
-  if (overload) {
-    inputExpanded = { ...process.env, ...expanded }
-  } else {
-    inputExpanded = { ...expanded, ...process.env }
-  }
-
-  const evaluatePlease = {
-    processEnv: {},
-    parsed: inputExpanded
-  }
-  const evaluated = dotenvEval.eval(evaluatePlease).parsed
 
   // but then for logging only log the original keys existing in parsed. this feels unnecessarily complex - like dotenv-expand should support the ability to inject additional `process.env` or objects as it sees fit to the object it wants to expand
   const result = {}
   for (const key in parsed) {
-    result[key] = evaluated[key]
+    result[key] = expanded[key]
   }
 
   return result
