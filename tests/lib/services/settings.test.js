8c540f68afd63a299889abeb54583fa97665afb4
Scott Motte <mot@mot.la>
2024-04-03 09:04:49 -0700

    settings

--- tests/lib/services/settings.test.js
+++ tests/lib/services/settings.test.js
@@ -1,4 +1,5 @@
 const t = require('tap')
+const sinon = require('sinon')
 
 const store = require('../../../src/shared/store')
 const Settings = require('../../../src/lib/services/settings')
@@ -7,6 +8,7 @@ const originalStore = store.confStore.store
 const originalPath = store.confStore.path
 
 t.beforeEach((ct) => {
+  store.confStore.path = '/tmp/.env'
   const mocked = {
     DOTENVX_TOKEN: 'dxo_1234',
     DOTENVX_HOSTNAME: 'https://mocked.dotenvx.com',
@@ -15,12 +17,16 @@ t.beforeEach((ct) => {
   }
 
   store.confStore.store = mocked
-  store.confStore.path = '/tmp/.env'
+
+  // do not delete. necessary to not write to user's real dotenvx settings .env file if running tests
+  sinon.stub(store.confStore, 'set').callsFake((key, value) => {
+    // protects from setting actually path/to/conf/settings/.env
+  })
 })
 
 t.afterEach((ct) => {
-  store.confStore.store = originalStore
   store.confStore.path = originalPath
+  store.confStore.store = originalStore
 })
 
 t.test('#run', ct => {
