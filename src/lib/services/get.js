7e8d0da809dc30461b006a8c9232e9fc2a289f5a
Scott Motte <mot@mot.la>
2024-04-05 10:09:28 -0700

    smartly display only what was parsed by default - unless --all flag passed

--- src/lib/services/get.js
+++ src/lib/services/get.js
@@ -14,68 +14,29 @@ class Get {
 
   run () {
     const processEnv = { ...process.env }
-
-    new Run(this.envs, this.overload, this.DOTENV_KEY, processEnv).run()
+    const { processedEnvs } = new Run(this.envs, this.overload, this.DOTENV_KEY, processEnv).run()
 
     if (!this.key) {
-      // // if user wants to return ALL envs (even prior set on machine)
-      // if (this.all) {
-      //   return expandedEnv
-      // }
+      // if user wants to return ALL envs (even prior set on machine)
+      if (this.all) {
+        return processEnv
+      }
 
-      // // typical scenario - return only envs that were identified in the .env file
-      // const result = {}
-      // for (const key of Object.keys(parsed)) {
-      //   result[key] = expandedEnv[key]
-      // }
+      // typical scenario - return only envs that were identified in the .env file
+      // iterate over all processedEnvs.parsed and grab from processEnv
+      const result = {}
+      for (const processedEnv of processedEnvs) {
+        // parsed means we saw the key in a file or --env flag. this effectively filters out any preset machine envs - while still respecting complex evaluating, expansion, and overload. in other words, the value might be the machine value because the key was displayed in a .env file
+        for (const key of Object.keys(processedEnv.parsed)) {
+          result[key] = processEnv[key]
+        }
+      }
 
-      return processEnv
-      // return result
+      return result
     }
 
     return processEnv[this.key]
   }
-
-  // constructor (key, envFile = '.env', overload = false, all = false) {
-  //   this.key = key
-  //   this.envFile = envFile
-  //   this.overload = overload
-  //   this.all = all
-  // }
-
-  // runOld () {
-  //   const clonedEnv = { ...process.env }
-  //   const options = {
-  //     processEnv: clonedEnv,
-  //     path: this.envFile,
-  //     override: this.overload
-  //   }
-  //   const parsed = dotenv.config(options).parsed
-
-  //   const expandedEnv = { ...clonedEnv }
-  //   const expandOptions = {
-  //     processEnv: expandedEnv,
-  //     parsed
-  //   }
-  //   dotenvExpand.expand(expandOptions)
-
-  //   if (!this.key) {
-  //     // if user wants to return ALL envs (even prior set on machine)
-  //     if (this.all) {
-  //       return expandedEnv
-  //     }
-
-  //     // typical scenario - return only envs that were identified in the .env file
-  //     const result = {}
-  //     for (const key of Object.keys(parsed)) {
-  //       result[key] = expandedEnv[key]
-  //     }
-
-  //     return result
-  //   }
-
-  //   return expandedEnv[this.key]
-  // }
 }
 
 module.exports = Get
