e758491f8de021c4e76b17d4eed1e7ee481607f7
Scott Motte <mot@mot.la>
2024-05-08 13:28:04 -0700

    add deprecation notice to all hub commands. they will be removed with the 1.0.0 release

--- src/cli/commands/hub.js
+++ src/cli/commands/hub.js
@@ -1,53 +1,89 @@
 const { Command } = require('commander')
 
 const store = require('./../../shared/store')
+const logger = require('./../../shared/logger')
 
 const hub = new Command('hub')
 
 hub
   .description('interact with dotenvx hub')
 
+const loginAction = require('./../actions/hub/login')
 hub
   .command('login')
   .description('authenticate to dotenvx hub')
   .option('-h, --hostname <url>', 'set hostname', store.getHostname())
-  .action(require('./../actions/hub/login'))
+  .action(function (...args) {
+    logger.warn('DEPRECATION NOTECE: [dotenvx hub login] will be removed in 1.0.0 release soon')
 
+    loginAction.apply(this, args)
+  })
+
+const pushAction = require('./../actions/hub/push')
 hub
   .command('push')
   .description('push .env.keys to dotenvx hub')
   .argument('[directory]', 'directory to push', '.')
   .option('-h, --hostname <url>', 'set hostname', store.getHostname())
-  .action(require('./../actions/hub/push'))
+  .action(function (...args) {
+    logger.warn('DEPRECATION NOTECE: [dotenvx hub push] will be removed in 1.0.0 release soon')
 
+    pushAction.apply(this, args)
+  })
+
+const pullAction = require('./../actions/hub/pull')
 hub
   .command('pull')
   .description('pull .env.keys from dotenvx hub')
   .argument('[directory]', 'directory to pull', '.')
   .option('-h, --hostname <url>', 'set hostname', store.getHostname())
-  .action(require('./../actions/hub/pull'))
+  .action(function (...args) {
+    logger.warn('DEPRECATION NOTECE: [dotenvx hub pull] will be removed in 1.0.0 release soon')
 
+    pullAction.apply(this, args)
+  })
+
+const openAction = require('./../actions/hub/open')
 hub
   .command('open')
   .description('view repository on dotenvx hub')
   .option('-h, --hostname <url>', 'set hostname', store.getHostname())
-  .action(require('./../actions/hub/open'))
+  .action(function (...args) {
+    logger.warn('DEPRECATION NOTECE: [dotenvx hub open] will be removed in 1.0.0 release soon')
 
+    openAction.apply(this, args)
+  })
+
+const tokenAction = require('./../actions/hub/token')
 hub
   .command('token')
   .description('print the auth token dotenvx hub is configured to use')
   .option('-h, --hostname <url>', 'set hostname', 'https://hub.dotenvx.com')
-  .action(require('./../actions/hub/token'))
+  .action(function (...args) {
+    logger.warn('DEPRECATION NOTECE: [dotenvx hub token] will be removed in 1.0.0 release soon')
 
+    tokenAction.apply(this, args)
+  })
+
+const statusAction = require('./../actions/hub/status')
 hub
   .command('status')
   .description('display logged in user')
-  .action(require('./../actions/hub/status'))
+  .action(function (...args) {
+    logger.warn('DEPRECATION NOTECE: [dotenvx hub status] will be removed in 1.0.0 release soon')
 
+    statusAction.apply(this, args)
+  })
+
+const logoutAction = require('./../actions/hub/logout')
 hub
   .command('logout')
   .description('log out this machine from dotenvx hub')
   .option('-h, --hostname <url>', 'set hostname', store.getHostname())
-  .action(require('./../actions/hub/logout'))
+  .action(function (...args) {
+    logger.warn('DEPRECATION NOTECE: [dotenvx hub logout] will be removed in 1.0.0 release soon')
+
+    logoutAction.apply(this, args)
+  })
 
 module.exports = hub
