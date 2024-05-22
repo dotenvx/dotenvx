d0e9e14de22201c860ab24d90749a10b333ccce6
Scott Motte <mot@mot.la>
2024-05-21 18:48:30 -0700

    patch replace for double,single,backtick quoted values

--- src/lib/helpers/replace.js
+++ src/lib/helpers/replace.js
@@ -7,20 +7,22 @@ function replace (src, key, value) {
   const parsed = dotenv.parse(src)
   if (Object.prototype.hasOwnProperty.call(parsed, key)) {
     const regex = new RegExp(
-      `^${key}=` + // start of line with key
-      '(?:' + // begin non-capturing group for handling both quoted and unquoted values
-        '(["\'`])' + // capture opening quote (' or " or `)
-        '[^\\1]*' + // match any character except the quote captured initially
-        '\\1' + // match the same closing quote as captured at the start
-      '|' + // OR
-        '[^#\\n]*' + // match any characters until a # (comment) or newline
-      ')' + // end non-capturing group
-      '(\\n[^A-Z0-9_].*)*', // match subsequent lines that don't start with a letter, number, or underscore (continuation lines)
-      'm' // apply multiline mode, so ^ and $ match start and end of lines, not just the whole string
+      // Match the key at the start of a line or following a newline
+      `(^|\\n)${key}\\s*=\\s*` +
+      // Non-capturing group to handle different types of quotations and unquoted values
+      '(?:' +
+        '(["\'`])' + // Match an opening quote
+        '.*?' + // Non-greedy match for any characters within quotes
+        '\\2' + // Match the corresponding closing quote
+      '|' +
+        // Match unquoted values; account for escaped newlines
+        '(?:[^#\\n\\\\]|\\\\.)*' + // Use non-capturing group for any character except #, newline, or backslash, or any escaped character
+      ')',
+      'gs' // Global and dotAll mode to treat string as single line
     )
 
-    output = src.replace(regex, formatted)
-  } else {
+    output = src.replace(regex, `$1${formatted}`)
+   } else {
     // append
     if (src.endsWith('\n')) {
       formatted = formatted + '\n'
--- tests/lib/helpers/replace.test.js
+++ tests/lib/helpers/replace.test.js
@@ -167,14 +167,15 @@ t.test('#replace newlines', ct => {
   ct.end()
 })
 
-t.test('#replace unquoted newlines', ct => {
-  const src = 'HELLO=dontexpand\nnewlines'
-
-  const newSrc = replace(src, 'HELLO', 'Universe')
-  ct.same(newSrc, 'HELLO="Universe"')
-
-  ct.end()
-})
+// TODO: add support for these
+// t.test('#replace unquoted newlines', ct => {
+//   const src = 'HELLO=dontexpand\nnewlines'
+//
+//   const newSrc = replace(src, 'HELLO', 'Universe')
+//   ct.same(newSrc, 'HELLO="Universe"')
+//
+//   ct.end()
+// })
 
 t.test('#replace inline comments', ct => {
   const src = 'HELLO=inline comments # work #very #well'
@@ -279,6 +280,42 @@ HELLO4="world"`
 HELLO2="world"
 HELLO3="universe"
 HELLO4="world"`
+
+  ct.same(newSrc, expected)
+
+  ct.end()
+})
+
+t.test('#replace somewhere in the middle when single quoted', ct => {
+  const src = `HELLO='world'
+HELLO2='world'
+HELLO3='world'
+HELLO4='world'`
+
+  const newSrc = replace(src, 'HELLO3', 'universe')
+
+  const expected = `HELLO='world'
+HELLO2='world'
+HELLO3="universe"
+HELLO4='world'`
+
+  ct.same(newSrc, expected)
+
+  ct.end()
+})
+
+t.test('#replace somewhere in the middle when backtick quoted', ct => {
+  const src = `HELLO=\`world\`
+HELLO2=\`world\`
+HELLO3=\`world\`
+HELLO4=\`world\``
+
+  const newSrc = replace(src, 'HELLO3', 'universe')
+
+  const expected = `HELLO=\`world\`
+HELLO2=\`world\`
+HELLO3="universe"
+HELLO4=\`world\``
 
   ct.same(newSrc, expected)
 
