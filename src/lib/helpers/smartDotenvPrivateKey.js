cfe08e46dfa977930ab30335f41d37b60d3c9d56
Scott Motte <mot@mot.la>
2024-07-04 07:25:48 -0700

    refactor for readability

--- src/lib/helpers/smartDotenvPrivateKey.js
+++ src/lib/helpers/smartDotenvPrivateKey.js
@@ -2,62 +2,83 @@ const fs = require('fs')
 const path = require('path')
 const dotenv = require('dotenv')
 
+const ENCODING = 'utf8'
 const PUBLIC_KEY_SCHEMA = 'DOTENV_PUBLIC_KEY'
 const PRIVATE_KEY_SCHEMA = 'DOTENV_PRIVATE_KEY'
 
 const guessPrivateKeyName = require('./guessPrivateKeyName')
 
-function smartDotenvPrivateKey (envFilepath) {
-  const privateKeyName = guessPrivateKeyName(envFilepath) // DOTENV_PRIVATE_KEY_${ENVIRONMENT}
-
-  // process.env wins
+function searchProcessEnv(privateKeyName) {
   if (process.env[privateKeyName] && process.env[privateKeyName].length > 0) {
     return process.env[privateKeyName]
   }
+}
 
-  // fallback to presence of .env.keys - path/to/.env.keys
+function searchKeysFile(privateKeyName, envFilepath) {
   const directory = path.dirname(envFilepath)
   const envKeysFilepath = path.resolve(directory, '.env.keys')
+
   if (fs.existsSync(envKeysFilepath)) {
-    const keysSrc = fs.readFileSync(envKeysFilepath)
+    const keysSrc = fs.readFileSync(envKeysFilepath, { encoding: ENCODING })
     const keysParsed = dotenv.parse(keysSrc)
 
     if (keysParsed[privateKeyName] && keysParsed[privateKeyName].length > 0) {
       return keysParsed[privateKeyName]
     }
   }
+}
 
-  // fallback to checking for presence of DOTENV_PUBLIC_KEY* in file and invert it
-  if (fs.existsSync(envFilepath)) {
-    const envSrc = fs.readFileSync(envFilepath)
-    const envParsed = dotenv.parse(envSrc)
+function invertForPrivateKeyName (envFilepath) {
+  if (!fs.existsSync(envFilepath)) {
+    return null
+  }
 
-    let publicKeyName
-    for (const keyName of Object.keys(envParsed)) {
-      if (keyName === PUBLIC_KEY_SCHEMA || keyName.startsWith(PUBLIC_KEY_SCHEMA)) {
-        publicKeyName = keyName
-      }
+  const envSrc = fs.readFileSync(envFilepath, { encoding: ENCODING })
+  const envParsed = dotenv.parse(envSrc)
+
+  let publicKeyName
+  for (const keyName of Object.keys(envParsed)) {
+    if (keyName === PUBLIC_KEY_SCHEMA || keyName.startsWith(PUBLIC_KEY_SCHEMA)) {
+      publicKeyName = keyName // find DOTENV_PUBLIC_KEY* in filename
+    }
+  }
+
+  if (publicKeyName) {
+    return publicKeyName.replace(PUBLIC_KEY_SCHEMA, PRIVATE_KEY_SCHEMA) // return inverted (DOTENV_PUBLIC_KEY* -> DOTENV_PRIVATE_KEY*) if found
+  }
+
+  return null
+}
+
+function smartDotenvPrivateKey (envFilepath) {
+  let privateKey = null
+  let privateKeyName = guessPrivateKeyName(envFilepath) // DOTENV_PRIVATE_KEY_${ENVIRONMENT}
+
+  // 1. attempt process.env first
+  privateKey = searchProcessEnv(privateKeyName)
+  if (privateKey) {
+    return privateKey
+  }
+
+  // 2. attempt .env.keys second (path/to/.env.keys)
+  privateKey = searchKeysFile(privateKeyName, envFilepath)
+  if (privateKey) {
+    return privateKey
+  }
+
+  // 3. attempt inverting `DOTENV_PUBLIC_KEY*` name inside file (unlocks custom filenames not matching .env.${ENVIRONMENT} pattern)
+  privateKeyName = invertForPrivateKeyName(envFilepath)
+  if (privateKeyName) {
+    // 3.1 attempt process.env first
+    privateKey = searchProcessEnv(privateKeyName)
+    if (privateKey) {
+      return privateKey
     }
 
-    if (publicKeyName) {
-      const privateKeyNameCalculated = publicKeyName.replace(PUBLIC_KEY_SCHEMA, PRIVATE_KEY_SCHEMA)
-
-      // process.env wins
-      if (process.env[privateKeyNameCalculated] && process.env[privateKeyNameCalculated].length > 0) {
-        return process.env[privateKeyNameCalculated]
-      }
-
-      // fallback to presence of .env.keys - path/to/.env.keys
-      const directory = path.dirname(envFilepath)
-      const envKeysFilepath = path.resolve(directory, '.env.keys')
-      if (fs.existsSync(envKeysFilepath)) {
-        const keysSrc = fs.readFileSync(envKeysFilepath)
-        const keysParsed = dotenv.parse(keysSrc)
-
-        if (keysParsed[privateKeyNameCalculated] && keysParsed[privateKeyNameCalculated].length > 0) {
-          return keysParsed[privateKeyNameCalculated]
-        }
-      }
+    // 3.2. attempt .env.keys second (path/to/.env.keys)
+    privateKey = searchKeysFile(privateKeyName, envFilepath)
+    if (privateKey) {
+      return privateKey
     }
   }
 
