66c951592a28084445708d7b337836b22b845687
Scott Motte <mot@mot.la>
2024-07-04 07:06:38 -0700

    smarter dotenv private key - find inverse of DOTENV_PUBLIC_KEY schema inside the passed file. this way filename can be adjusted/customized

--- src/lib/helpers/smartDotenvPrivateKey.js
+++ src/lib/helpers/smartDotenvPrivateKey.js
@@ -2,6 +2,9 @@ const fs = require('fs')
 const path = require('path')
 const dotenv = require('dotenv')
 
+const PUBLIC_KEY_SCHEMA = 'DOTENV_PUBLIC_KEY'
+const PRIVATE_KEY_SCHEMA = 'DOTENV_PRIVATE_KEY'
+
 const guessPrivateKeyName = require('./guessPrivateKeyName')
 
 function smartDotenvPrivateKey (envFilepath) {
@@ -21,6 +24,40 @@ function smartDotenvPrivateKey (envFilepath) {
 
     if (keysParsed[privateKeyName] && keysParsed[privateKeyName].length > 0) {
       return keysParsed[privateKeyName]
+    }
+  }
+
+  // fallback to checking for presence of DOTENV_PUBLIC_KEY* in file and invert it
+  if (fs.existsSync(envFilepath)) {
+    const envSrc = fs.readFileSync(envFilepath)
+    const envParsed = dotenv.parse(envSrc)
+
+    let publicKeyName
+    for (const keyName of Object.keys(envParsed)) {
+      if (keyName === PUBLIC_KEY_SCHEMA || keyName.startsWith(PUBLIC_KEY_SCHEMA)) {
+        publicKeyName = keyName
+      }
+    }
+
+    if (publicKeyName) {
+      const privateKeyNameCalculated = publicKeyName.replace(PUBLIC_KEY_SCHEMA, PRIVATE_KEY_SCHEMA)
+
+      // process.env wins
+      if (process.env[privateKeyNameCalculated] && process.env[privateKeyNameCalculated].length > 0) {
+        return process.env[privateKeyNameCalculated]
+      }
+
+      // fallback to presence of .env.keys - path/to/.env.keys
+      const directory = path.dirname(envFilepath)
+      const envKeysFilepath = path.resolve(directory, '.env.keys')
+      if (fs.existsSync(envKeysFilepath)) {
+        const keysSrc = fs.readFileSync(envKeysFilepath)
+        const keysParsed = dotenv.parse(keysSrc)
+
+        if (keysParsed[privateKeyNameCalculated] && keysParsed[privateKeyNameCalculated].length > 0) {
+          return keysParsed[privateKeyNameCalculated]
+        }
+      }
     }
   }
 
