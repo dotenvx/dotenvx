6fe8612e44e65d6a2e3b053701be698d739059d1
Scott Motte <mot@mot.la>
2024-07-15 18:23:50 -0700

    add newline

--- src/cli/dotenvx.js
+++ src/cli/dotenvx.js
@@ -30,7 +30,7 @@ program
     setLogLevel(options)
   })
 
-program.addHelpText('after', '  pro                               🏆 pro')
+program.addHelpText('after', '  pro                               🏆 pro\n')
 
 program
   .argument('[command]', 'dynamic command')
