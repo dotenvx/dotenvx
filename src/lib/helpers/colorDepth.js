0bd1f6cf69afe02f8159695acdac7720f88cc4a1
Scott Motte <mot@mot.la>
2024-08-27 20:43:00 -0700

    fallback to TERM

--- src/lib/helpers/colorDepth.js
+++ src/lib/helpers/colorDepth.js
@@ -4,6 +4,12 @@ const getColorDepth = () => {
   try {
     return WriteStream.prototype.getColorDepth()
   } catch (error) {
+    const term = process.env.TERM
+
+    if (term && (term.includes('256color') || term.includes('xterm'))) {
+      return 8 // 256 colors
+    }
+
     return 4
   }
 }
