dc55b234c3142cb0e63c45e358187c6880b9dbc6
Daniel Lando <daniel.sorridi@gmail.com>
2024-09-17 14:35:22 +0000

    fix: improve test

--- tests/cli/test_pkg_bug.sh
+++ tests/cli/test_pkg_bug.sh
@@ -1,9 +1,18 @@
+cd "$(dirname "$0")"
 cd ../../
+
 rm -rf bin
 npx pkg . --no-bytecode --public-packages "*" --public --target node20-linuxstatic-x64 --output bin/linux-amd64/dotenvx
 cd bin/linux-amd64/
 echo "HELLO=Ubuntu" > .env
 echo "console.log('Hello ' + process.env.HELLO)" > index.js
-./dotenvx run --quiet -- ./dotenvx run --quiet -- ./dotenvx run --quiet -- node index.js
+RESULT=$(./dotenvx run --quiet -- ./dotenvx run --quiet -- ./dotenvx run --quiet -- node index.js)
 rm -rf bin
-cd -
+echo $RESULT
+if [ "$RESULT" == "Hello Ubuntu" ]; then
+  echo "Test passed"
+  exit 0
+else
+  echo "Test failed"
+  exit 1
+fi
