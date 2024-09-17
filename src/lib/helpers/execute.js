8eaf5b4758b987c9a19bf799d2800a6deb7399a1
Daniel Lando <daniel.sorridi@gmail.com>
2024-09-17 15:47:29 +0000

    fix: ignore coverage from pkg case

--- src/lib/helpers/execute.js
+++ src/lib/helpers/execute.js
@@ -1,5 +1,7 @@
 const execa = require('execa')
+/* c8 ignore start */
 const pkgArgs = process.pkg ? { PKG_EXECPATH: '' } : {}
+/* c8 ignore stop */
 
 const execute = {
   execa (command, args, options) {
