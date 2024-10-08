c339a1f9ee8ed9242e7075dac7a67473d4c6aa9d
immeëmosol <will+developer@willfris.nl>
2024-10-08 20:15:47 +0200

    standard --fix

--- src/lib/helpers/filesystem.js
+++ src/lib/helpers/filesystem.js
@@ -1,36 +1,36 @@
 const fs = require('fs')
 
 const lax = function (callback) {
-    return function relax() {
-        try {
-            //const result
-            //    = fs.existsSync(arguments[0])
-            //    ? callback(...arguments)
-            //    : null;
-            const result = callback(...arguments);
+  return function relax () {
+    try {
+      // const result
+      //    = fs.existsSync(arguments[0])
+      //    ? callback(...arguments)
+      //    : null;
+      const result = callback(...arguments)
 
-            return result;
-        } catch (error) {
-            if ('ENOENT' === error.code) {
-                return;
-            }
-            throw error;
-        }
-    };
-};
+      return result
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        return
+      }
+      throw error
+    }
+  }
+}
 const filesystem = {
-    appendFileLaxSync: lax(fs.appendFileSync),
-    appendFileSync: fs.appendFileSync,
-    chmodLaxSync: lax(fs.chmodSync),
-    chmodSync: fs.chmodSync,
-    existsLaxSync: lax(fs.existsSync),
-    existsSync: fs.existsSync,
-    readFileLaxSync: lax(fs.readFileSync),
-    readFileSync: fs.readFileSync,
-    readdirLaxSync: lax(fs.readdirSync),
-    readdirSync: fs.readdirSync,
-    writeFileLaxSync: lax(fs.writeFileSync),
-    writeFileSync: fs.writeFileSync,
-};
+  appendFileLaxSync: lax(fs.appendFileSync),
+  appendFileSync: fs.appendFileSync,
+  chmodLaxSync: lax(fs.chmodSync),
+  chmodSync: fs.chmodSync,
+  existsLaxSync: lax(fs.existsSync),
+  existsSync: fs.existsSync,
+  readFileLaxSync: lax(fs.readFileSync),
+  readFileSync: fs.readFileSync,
+  readdirLaxSync: lax(fs.readdirSync),
+  readdirSync: fs.readdirSync,
+  writeFileLaxSync: lax(fs.writeFileSync),
+  writeFileSync: fs.writeFileSync
+}
 
-module.exports = filesystem;
+module.exports = filesystem
