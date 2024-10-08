44096ec2a6905243a9542592508c17ae09202fce
immeëmosol <will+developer@willfris.nl>
2024-10-08 19:59:19 +0200

    Add relaxed fs calls

--- src/lib/helpers/filesystem.js
+++ src/lib/helpers/filesystem.js
@@ -1,11 +1,35 @@
 const fs = require('fs')
 
+const lax = function (callback) {
+    return function relax() {
+        try {
+            //const result
+            //    = fs.existsSync(arguments[0])
+            //    ? callback(...arguments)
+            //    : null;
+            const result = callback(...arguments);
+
+            return result;
+        } catch (error) {
+            if ('ENOENT' === error.code) {
+                return;
+            }
+            throw error;
+        }
+    };
+};
 const filesystem = {
+    appendFileLaxSync: lax(fs.appendFileSync),
     appendFileSync: fs.appendFileSync,
+    chmodLaxSync: lax(fs.chmodSync),
     chmodSync: fs.chmodSync,
+    existsLaxSync: lax(fs.existsSync),
     existsSync: fs.existsSync,
+    readFileLaxSync: lax(fs.readFileSync),
     readFileSync: fs.readFileSync,
+    readdirLaxSync: lax(fs.readdirSync),
     readdirSync: fs.readdirSync,
+    writeFileLaxSync: lax(fs.writeFileSync),
     writeFileSync: fs.writeFileSync,
 };
 
