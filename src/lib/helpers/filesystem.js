0411b9c68cad8f2306cf1c2b9948a3b0e0b342c8
Scott Motte <mot@mot.la>
2024-10-11 17:05:42 -0700

    remove lax

--- src/lib/helpers/filesystem.js
+++ src/lib/helpers/filesystem.js
@@ -1,35 +1,11 @@
 const fs = require('fs')
 
-const lax = function (callback) {
-  return function relax () {
-    try {
-      // const result
-      //    = fs.existsSync(arguments[0])
-      //    ? callback(...arguments)
-      //    : null;
-      const result = callback(...arguments)
-
-      return result
-    } catch (error) {
-      if (error.code === 'ENOENT') {
-        return
-      }
-      throw error
-    }
-  }
-}
 const filesystem = {
-  appendFileLaxSync: lax(fs.appendFileSync),
   appendFileSync: fs.appendFileSync,
-  chmodLaxSync: lax(fs.chmodSync),
   chmodSync: fs.chmodSync,
-  existsLaxSync: lax(fs.existsSync),
   existsSync: fs.existsSync,
-  readFileLaxSync: lax(fs.readFileSync),
   readFileSync: fs.readFileSync,
-  readdirLaxSync: lax(fs.readdirSync),
   readdirSync: fs.readdirSync,
-  writeFileLaxSync: lax(fs.writeFileSync),
   writeFileSync: fs.writeFileSync
 }
 
