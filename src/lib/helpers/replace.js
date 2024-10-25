55fa12fcedba39c97c57a46c69db1750c1115c1b
Scott Motte <mot@mot.la>
2024-10-25 13:54:59 -0700

    remove escapedValue logic - better handled with smart quotes

--- src/lib/helpers/replace.js
+++ src/lib/helpers/replace.js
@@ -7,26 +7,12 @@ const escapeDollarSigns = require('./escapeDollarSigns')
 
 function replace (src, key, replaceValue) {
   let output
-  let escapedValue = util.inspect(replaceValue, { showHidden: false, depth: null, colors: false })
-
-  if (replaceValue.includes('\n')) {
-    escapedValue = JSON.stringify(replaceValue) // use JSON stringify if string contains newlines
-    escapedValue = escapedValue.replace(/\\n/g, '\n') // fix up newlines
-    escapedValue = escapedValue.replace(/\\r/g, '\r')
-  }
-
-  // prevents test\test (and similar) from becoming test\\test and then test\\\\test, etc recursively after each encrypt/decrypt combo
-  if (replaceValue.includes('\\')) {
-    escapedValue = escapedValue.replace(/\\\\/g, '\\')
-  }
-
   let newPart = ''
 
   const parsed = dotenv.parse(src)
   const _quotes = quotes(src)
   if (Object.prototype.hasOwnProperty.call(parsed, key)) {
     const quote = _quotes[key]
-    // newPart += `${key}=${quote}${escapedValue}${quote}`
     newPart += `${key}=${quote}${replaceValue}${quote}`
 
     const originalValue = parsed[key]
@@ -58,7 +44,7 @@ function replace (src, key, replaceValue) {
     // $2 preserves export
     output = src.replace(currentPart, `$1$2${saferInput}`)
   } else {
-    newPart += `${key}=${escapedValue}`
+    newPart += `${key}="${replaceValue}"`
 
     // append
     if (src.endsWith('\n')) {
