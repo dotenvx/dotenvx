f629f1fb898f9a45417525d5ff11100f42dd819e
Scott Motte <mot@mot.la>
2024-11-13 14:21:03 -0800

    try removing

--- src/lib/helpers/execute.js
+++ src/lib/helpers/execute.js
@@ -1,11 +1,8 @@
 const execa = require('execa')
-/* c8 ignore start */
-const pkgArgs = process.pkg ? { PKG_EXECPATH: '' } : {}
-/* c8 ignore stop */
 
 const execute = {
   execa (command, args, options) {
-    return execa(command, args, { ...options, env: { ...options.env, ...pkgArgs } })
+    return execa(command, args, options)
   }
 }
 
