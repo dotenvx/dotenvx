15e665cecb91f7b47d8f0914dd2d941854f967d2
Scott Motte <mot@mot.la>
2024-11-22 08:22:28 -0800

    standard formatting 🧼

--- tests/lib/helpers/parse.test.js
+++ tests/lib/helpers/parse.test.js
@@ -719,10 +719,15 @@ ANOTHER_VAR="$\{SOME_VAR} is $\{VAR_WITH_LITERAL_VALUE}"
 
   const { parsed } = new Parse(src, null, process.env, true).run()
 
+  /* eslint-disable no-template-curly-in-string */
+  const varWithLiteralValue = '${THIS_IS_A_LITERAL_VALUE} empty'
+  const anotherVar = 'Why is ${THIS_IS_A_LITERAL_VALUE} empty'
+  /* eslint-enable no-template-curly-in-string */
+
   ct.same(parsed, {
-    VAR_WITH_LITERAL_VALUE: '${THIS_IS_A_LITERAL_VALUE} empty',
+    VAR_WITH_LITERAL_VALUE: varWithLiteralValue,
     SOME_VAR: 'Why',
-    ANOTHER_VAR: 'Why is ${THIS_IS_A_LITERAL_VALUE} empty'
+    ANOTHER_VAR: anotherVar
   })
 
   ct.end()
