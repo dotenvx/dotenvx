13649d98f2aecd9882702dcd32efca0a9373a3fd
Nick Corin <nickcorin@gmail.com>
2024-12-22 20:02:24 +0200

    precommit: add HEAD argument to git diff

--- src/lib/services/precommit.js
+++ src/lib/services/precommit.js
@@ -88,7 +88,7 @@ class Precommit {
 
   _isFileToBeCommitted (filePath) {
     try {
-      const output = childProcess.execSync('git diff --name-only').toString()
+      const output = childProcess.execSync('git diff HEAD --name-only').toString()
       const files = output.split('\n')
 
       return files.includes(filePath)
