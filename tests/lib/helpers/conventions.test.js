df2b3d2781088bdd2dec4f771570565c21008c2f
Scott Motte <mot@mot.la>
2025-03-16 15:10:06 -0700

    add tests

--- tests/lib/helpers/conventions.test.js
+++ tests/lib/helpers/conventions.test.js
@@ -92,15 +92,31 @@ t.test('#conventions flow (process.env.NODE_ENV is test)', ct => {
   ct.end()
 })
 
-// t.test('#conventions (process.env.NODE_ENV is unrecognized)', ct => {
-//   process.env.NODE_ENV = 'unrecognized'
-//
-//   const envs = conventions('nextjs')
-//
-//   ct.same(envs, [
-//     { type: 'envFile', value: '.env.local' },
-//     { type: 'envFile', value: '.env' }
-//   ])
-//
-//   ct.end()
-// })
+t.test('#conventions (process.env.NODE_ENV is unrecognized)', ct => {
+  process.env.NODE_ENV = 'unrecognized'
+
+  const envs = conventions('nextjs')
+
+  ct.same(envs, [
+    { type: 'envFile', value: '.env.local' },
+    { type: 'envFile', value: '.env' }
+  ])
+
+  ct.end()
+})
+
+t.test('#conventions flow (process.env.NODE_ENV is unrecognized)', ct => {
+  process.env.NODE_ENV = 'unrecognized'
+
+  const envs = conventions('flow')
+
+  ct.same(envs, [
+    { type: 'envFile', value: '.env.unrecognized.local' },
+    { type: 'envFile', value: '.env.unrecognized' },
+    { type: 'envFile', value: '.env.local' },
+    { type: 'envFile', value: '.env' },
+    { type: 'envFile', value: '.env.defaults' }
+  ])
+
+  ct.end()
+})
