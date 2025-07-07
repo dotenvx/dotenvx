d961270e196189db2ad75c5e84fabbfa0008af45
Scott Motte <mot@mot.la>
2025-07-07 12:24:33 -0700

    standard formatting 🧼

--- tests/lib/helpers/errors.test.js
+++ tests/lib/helpers/errors.test.js
@@ -3,7 +3,7 @@ const t = require('tap')
 const Errors = require('../../../src/lib/helpers/errors')
 
 t.test('#errors', ct => {
-  const result = new Errors({message: 'hi'}).dangerousDependencyHoist()
+  const result = new Errors({ message: 'hi' }).dangerousDependencyHoist()
 
   t.equal(result.code, 'DANGEROUS_DEPENDENCY_HOIST')
   t.equal(result.message, '[DANGEROUS_DEPENDENCY_HOIST] your environment has hoisted an incompatible version of a dotenvx dependency: hi')
