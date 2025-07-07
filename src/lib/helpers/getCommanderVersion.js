81cc0c4f3c845bfe7546403ad461b69fd4a40917
Scott Motte <mot@mot.la>
2025-07-07 12:12:10 -0700

    standard formatting 🧼

--- src/lib/helpers/getCommanderVersion.js
+++ src/lib/helpers/getCommanderVersion.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 
-function getCommanderVersion() {
+function getCommanderVersion () {
   const commanderMain = require.resolve('commander')
   const pkgPath = path.join(commanderMain, '..', 'package.json')
   return JSON.parse(fs.readFileSync(pkgPath, 'utf8')).version
