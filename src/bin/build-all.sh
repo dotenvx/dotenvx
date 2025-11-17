ab1ebba69274e8a9c3506d18edb9c6881ff17287
Sebastian Danielsson <sebastian.danielsson@trafikverket.se>
2025-11-17 12:33:01 +0100

    Add standalone build with Bun Single-file executable

---
+++ src/bin/build-all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+targets=(
+  "bun-darwin-arm64-modern:dotenvx-darwin-arm64-modern"
+  "bun-darwin-x64-modern:dotenvx-darwin-x64-modern"
+  "bun-linux-arm64-modern:dotenvx-linux-arm64-modern"
+  "bun-linux-x64-modern:dotenvx-linux-x64-modern"
+  "bun-windows-x64-modern:dotenvx-windows-x64-modern.exe"
+)
+
+for target_pair in "${targets[@]}"; do
+  target="${target_pair%%:*}"
+  output="${target_pair##*:}"
+  echo "Building for $target..."
+  bun build --compile --minify --sourcemap --bytecode --target="$target" ./src/bin/standalone.ts --outfile "./dist/$output"
+done
