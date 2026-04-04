64bc6bb73105c41a0d305c52522983b0d4e0cc5c
Scott Motte <mot@mot.la>
2026-04-03 18:00:35 -0700

    test coverage

---
+++ tests/lib/services/run.sync.test.js
@@ -0,0 +1,49 @@
+const t = require('tap')
+const fs = require('fs')
+const os = require('os')
+const path = require('path')
+const proxyquire = require('proxyquire').noCallThru()
+
+const Run = require('../../../src/lib/services/run')
+
+t.beforeEach(() => {
+  process.env = {}
+})
+
+t.test('#runSync processes inline env strings', ct => {
+  const cwd = process.cwd()
+  const tmpdir = fs.mkdtempSync(path.join(os.tmpdir(), 'dotenvx-runsync-'))
+  process.chdir(tmpdir)
+
+  const result = new Run([{ type: 'env', value: 'HELLO=sync' }]).runSync()
+  const envRow = result.processedEnvs.find((row) => row.type === 'env')
+
+  t.same(envRow, {
+    type: 'env',
+    string: 'HELLO=sync',
+    parsed: { HELLO: 'sync' },
+    errors: [],
+    injected: { HELLO: 'sync' },
+    preExisted: {}
+  })
+  t.same(result.uniqueInjectedKeys, ['HELLO'])
+
+  process.chdir(cwd)
+  ct.end()
+})
+
+t.test('#runSync records non-ENOENT env file read errors', ct => {
+  const RunWithSyncError = proxyquire('../../../src/lib/services/run', {
+    './../helpers/detectEncodingSync': () => {
+      throw new Error('Mock Sync Error')
+    }
+  })
+
+  const result = new RunWithSyncError([{ type: 'envFile', value: '.env' }]).runSync()
+
+  t.equal(result.processedEnvs.length, 1)
+  t.equal(result.processedEnvs[0].type, 'envFile')
+  t.equal(result.processedEnvs[0].filepath, '.env')
+  t.equal(result.processedEnvs[0].errors[0].message, 'Mock Sync Error')
+  ct.end()
+})
