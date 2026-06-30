17b0a05123e22d3e950fa4dce126b174ffa2d65b
Scott Motte <mot@mot.la>
2026-06-30 10:35:36 -0700

    remove - unused

--- src/lib/helpers/keyResolution/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  readProcessKey: require('./readProcessKey'),
-  readFileKey: require('./readFileKey'),
-  readFileKeySync: require('./readFileKeySync'),
-  guessPrivateKeyFilename: require('./../guessPrivateKeyFilename'),
-  dotenvPrivateKeyNames: require('./../dotenvPrivateKeyNames')
-}
