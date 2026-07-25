7cd2ef8cd3f71f2c76eec1ed240d4c6d04e7acce
Scott Motte <mot@mot.la>
2026-07-25 10:53:46 +0200

    build api error

--- src/lib/helpers/buildApiError.js
+++ src/lib/helpers/buildApiError.js
@@ -1,12 +1,15 @@
 function buildApiError (statusCode, json) {
   const code = json.error.code || statusCode.toString()
   const message = `[${code}] ${json.error.message}`
-  const help = `[${code}] ${json.error.help || JSON.stringify(json)}`
+  const help = json.error.help ? `fix: [${json.error.help}]` : `[${code}] ${JSON.stringify(json)}`
   const meta = json.error.meta
 
   const error = new Error(message)
   error.code = code
   error.help = help
+  if (json.error.help) {
+    error.messageWithHelp = `${message}. ${help}`
+  }
   error.meta = meta
   error.json = json
 
