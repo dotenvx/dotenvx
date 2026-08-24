d31ba858ec5a75c1f07eba370f6afb2e735e7c8b
Scott Motte <mot@mot.la>
2026-08-24 09:49:22 -0700

    handle both error codes

--- src/lib/helpers/isTeamRequiredError.js
+++ src/lib/helpers/isTeamRequiredError.js
@@ -1,5 +1,10 @@
+const TEAM_REQUIRED_CODES = new Set([
+  'TEAM_REQUIRED',
+  'DOTENVX_TEAM_REQUIRED'
+])
+
 function isTeamRequiredError (error) {
-  return Boolean(error && error.code === 'TEAM_REQUIRED')
+  return Boolean(error && TEAM_REQUIRED_CODES.has(error.code))
 }
 
 module.exports = isTeamRequiredError
